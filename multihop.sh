e7cf849db51c9bc11a164798cc2b0584f5a9dc8a
Pieter Staal <pieterstaal@outlook.com>
2016-11-08 15:51:33 +0100

    Add counter for sequential testing

--- multihop.sh
+++ multihop.sh
@@ -1,6 +1,10 @@
-set counter to 0.
+declare parameter startheight is 100.
+
+set counter to startheight.
 until False
 {
-    run hop.sh(350,5).
-    set counter to counter + 1.
+    run hop.sh(200,1).
+    set counter to counter + 100.
+    print "Relaunching to " + counter + " meters in a few secs".
+    wait 3.
 }
