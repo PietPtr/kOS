90f783391842060eb62329af5aa08abc5f241300
Pieter Staal <pieterstaal@outlook.com>
2016-11-09 00:25:52 +0100

    Add very basic hohmann transfer implementation
    Only to go to a higher orbit, ignores Principia.

--- hohmann.sh
+++ hohmann.sh
@@ -1,5 +1,5 @@
-declare parameter periaps is 500000.
-declare parameter apoaps is  500000.
+declare parameter periapsis is 500000.
+declare parameter apoapsis is  500000.
 
 set ship:control:pilotmainthrottle to 0.
 
@@ -7,6 +7,34 @@ rcs on.
 
 clearscreen.
 
-lock throttle to mythrottle.
-lock steering to Up + R(0,0,180).
+lock throttle to 0.
+lock steering to ship:prograde.
+
+run warppe.sh.
+
+until kuniverse:timewarp:rate = 1 { wait 0.1. }
+
+wait 5.
+
+until ship:apoapsis >= apoapsis
+{
+    lock throttle to 0.2.
+}
+
+lock throttle to 0.
+
+until throttle = 0 { wait 0.1. }
+
+wait 1.
+
+run warpap.sh.
+
+until kuniverse:timewarp:rate = 1 { wait 0.1. }
+
+wait 5.
+
+until ship:periapsis >= periapsis - 20000
+{
+    lock throttle to 0.2.
+}
 
