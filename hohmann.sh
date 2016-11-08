76516e8bcc0da65342b77e6a2ded50549d9ec656
Pieter Staal <pieterstaal@outlook.com>
2016-11-08 20:26:58 +0100

    Add hohmann, warp to peri, and warp to ap scripts

---
+++ hohmann.sh
@@ -0,0 +1,12 @@
+declare parameter periaps is 500000.
+declare parameter apoaps is  500000.
+
+set ship:control:pilotmainthrottle to 0.
+
+rcs on.
+
+clearscreen.
+
+lock throttle to mythrottle.
+lock steering to Up + R(0,0,180).
+
